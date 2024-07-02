208bbb287de7039b3f444fed37c958faf6d22278
Rei Tan <tanreiyi@gmail.com>
2024-07-02 17:06:51 -0500

    display TextBox

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,39 +1,11 @@
 import React from "react"; 
-import { useEffect, useContext } from "react"; 
-import { AuthContext } from "../../AuthContext";
-import { useNavigate } from "react-router-dom"; 
 import "./HomePage.css";
-
+import { TextBox } from "../../components/TextBox";
 
 export const HomePage = () => {
-    const { authState, setAuthState } = useContext(AuthContext);
-    const navigate = useNavigate(); 
-    useEffect(() => {
-        if (authState) {
-            fetch("http://localhost:4000/auth/verify", {
-                method: "POST", 
-                headers: {
-                    "Content-Type": "application/json" 
-                },
-                body: JSON.stringify({
-                    token: authState
-                })
-            })
-            .then(res => res.json())
-            .then(data => {
-                if (!data.verified) {
-                    localStorage.remove("token"); 
-                    setAuthState(null);
-                    navigate("/login");
-                }
-            })
-            .catch(error => console.error(error)); 
-        }
-        else {
-            navigate("/login");
-        }
-    }, [authState]);
     return (
-        <div>homepage</div>
+        <>
+            <TextBox />
+        </>
     );
 }
