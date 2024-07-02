3e5a25ba52515b9deeba19978be6df97c3d56509
Rei Tan <tanreiyi@gmail.com>
2024-07-02 17:06:33 -0500

    export PrivateRoute and FirebaseProvider

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { TextBox } from "./TextBox";
 export { TextForm } from "./TextForm";
 export { Button } from "./Button"; 
 export { WarningMessage } from "./WarningMessage";
+export { PrivateRoute } from "./PrivateRoute";
+export { FirebaseProvider, useFirebase } from "./FirebaseProvider";
