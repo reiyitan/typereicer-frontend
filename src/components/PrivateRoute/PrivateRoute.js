d5a82b6709bb7c8454206bf081320409cc2cda1c
Rei Tan <tanreiyi@gmail.com>
2024-07-02 19:46:30 -0500

    style loading screen

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./PrivateRoute.css";
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom"; 
 import { useFirebase } from "../FirebaseProvider";
