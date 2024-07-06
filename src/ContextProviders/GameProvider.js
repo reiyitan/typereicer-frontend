306d613608018da0dfca78330f146d07d362337b
Rei Tan <tanreiyi@gmail.com>
2024-07-06 00:44:35 -0500

    context provider for game variables

--- src/ContextProviders/GameProvider.js
+++ src/ContextProviders/GameProvider.js
@@ -1,12 +1,35 @@
 import React from "react"; 
-import { useState, createContext, useContext } from "react"; 
+import { useState, createContext, useContext, useEffect } from "react"; 
 
 const GameContext = createContext();
 export const GameProvider = ({children}) => {
-    const [words, setWords] = useState([]); 
-    const [typedWords, setTypedWords] = useState([]); 
+    const [words, setWords] = useState(new Array(50).fill("")); 
+    const [typedWords, setTypedWords] = useState(new Array(50).fill("")); 
     const [numWords, setNumWords] = useState(50);
-    const [wordIndex, setWordIndex] = useState(0); 
+    const [currWordIndex, setCurrWordIndex] = useState(0); 
+    const [currCharIndex, setCurrCharIndex] = useState(0);
+    const [indexArray, setIndexArray] = useState(Array.from({ length: 50 }, (_, index) => index)); 
+    const [prevCharRef, setPrevCharRef] = useState(null);
+    const [currCharRef, setCurrCharRef] = useState(null);
+
+    useEffect(() => {
+        setIndexArray(Array.from({ length: numWords }, (_, index) => index));
+    }, [numWords]); 
+
+    const fetchWords = () => {
+        setCurrWordIndex(0);
+        fetch(`http://localhost:4000/words/get_words?num_words=${numWords}`)
+        .then(res => res.json())
+        .then(data => {
+            setWords(data.words);
+            setTypedWords(new Array(data.words.length).fill(""));
+            setCurrWordIndex(0); 
+            setCurrCharIndex(0); 
+            setPrevCharRef(null);
+            setCurrCharRef(null);
+     })
+        .catch(error => console.error(error));
+    }
 
     return (
         <GameContext.Provider
@@ -14,7 +37,12 @@ export const GameProvider = ({children}) => {
                 words, setWords,
                 typedWords, setTypedWords,
                 numWords, setNumWords, 
-                wordIndex, setWordIndex
+                fetchWords,
+                currWordIndex, setCurrWordIndex,
+                currCharIndex, setCurrCharIndex,
+                indexArray,
+                prevCharRef, setPrevCharRef,
+                currCharRef, setCurrCharRef
             }}
         >
             {children}
