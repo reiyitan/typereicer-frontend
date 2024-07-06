ad706cee02b4b9d11fb0aceabf7d65d4b480ff30
Rei Tan <tanreiyi@gmail.com>
2024-07-06 16:56:53 -0500

    move handleKeyDown into GameProvider

--- src/ContextProviders/GameProvider.js
+++ src/ContextProviders/GameProvider.js
@@ -12,14 +12,29 @@ export const GameProvider = ({children}) => {
     const [indexArray, setIndexArray] = useState(Array.from({ length: defaultNumWords }, (_, index) => index)); 
     const [prevCharRef, setPrevCharRef] = useState(null);
     const [currCharRef, setCurrCharRef] = useState(null);
-    const [gameRunning, setGameRunning] = useState(null);
+    const [gameFocused, setGameFocused] = useState(null);
+    const [timerRunning, setTimerRunning] = useState(false);
+    const [seconds, setSeconds] = useState(0);
 
     const handleGameEnd = () => {
-        setGameRunning(false);
+        setTimerRunning(false);
         console.log("game end");
     }
 
-
+    useEffect(() => {
+        let interval = null;
+        const startTime = Date.now();
+        if (timerRunning) {
+            interval = setInterval(() => {
+                const currentTime = Date.now();
+                const elapsedTime = (currentTime - startTime) / 1000;
+                setSeconds(elapsedTime);
+            }, 10);
+        } else if (!timerRunning && seconds !== 0) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [timerRunning]);
 
     useEffect(() => {
         setIndexArray(Array.from({ length: numWords }, (_, index) => index));
@@ -40,8 +55,50 @@ export const GameProvider = ({children}) => {
             setCurrCharIndex(0); 
             setPrevCharRef(null);
             setCurrCharRef(null);
+            setTimerRunning(false); 
+            setSeconds(0);
         })
         .catch(error => console.error(error));
+    }
+
+    const handleKeyDown = (e) => {
+        if (!gameFocused) return;
+        if (e.ctrlKey && e.key === "Backspace") {
+            const newTypedWords = [...typedWords]; 
+            newTypedWords[currWordIndex] = ""; 
+            setTypedWords(newTypedWords);
+            setCurrCharIndex(0);
+        }
+        else if (e.key === "Backspace") {
+            const targetWord = typedWords[currWordIndex]; 
+            const newTypedWord = targetWord.substring(0, targetWord.length - 1); 
+            const newTypedWords = [...typedWords]; 
+            newTypedWords[currWordIndex] = newTypedWord; 
+            setTypedWords(newTypedWords); 
+            setCurrCharIndex(prevIndex => Math.max(0, prevIndex - 1));
+        }
+        else if (e.key === " " ) {
+            if (currWordIndex === numWords - 1) {
+                handleGameEnd();
+                return;
+            }
+            if (typedWords[currWordIndex] === "") return;
+            setCurrWordIndex(prevIndex => prevIndex + 1);
+            setCurrCharIndex(0);
+        }
+        else if ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 65 && e.keyCode <= 90)) {
+            if (!timerRunning && currWordIndex === 0 && currCharIndex === 0) setTimerRunning(true);
+            if (typedWords[currWordIndex].length > words[currWordIndex].length + 5) return;
+            if (!gameFocused) setGameFocused(true);
+            const newTypedWords = [...typedWords]; 
+            newTypedWords[currWordIndex] += e.key; 
+            setTypedWords(newTypedWords);
+            setCurrCharIndex(prevIndex => prevIndex + 1);
+            if (currWordIndex === numWords - 1 && newTypedWords[currWordIndex] === words[currWordIndex]) {
+                handleGameEnd(); 
+                return;
+            }
+        }
     }
 
     return (
@@ -56,8 +113,11 @@ export const GameProvider = ({children}) => {
                 indexArray,
                 prevCharRef, setPrevCharRef,
                 currCharRef, setCurrCharRef,
-                gameRunning, setGameRunning,
-                handleGameEnd
+                gameFocused, setGameFocused,
+                handleKeyDown,
+                handleGameEnd,
+                timerRunning, setTimerRunning, 
+                seconds, setSeconds
             }}
         >
             {children}
