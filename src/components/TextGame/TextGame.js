c337f7d9f55de944d68b57a5ec2731c28a67492f
Rei Tan <tanreiyi@gmail.com>
2024-07-17 19:14:33 -0500

    change min acc

--- src/components/TextGame/TextGame.js
+++ src/components/TextGame/TextGame.js
@@ -73,7 +73,7 @@ const ResultPanel = () => {
             <p className="results-value">{wpm}</p>
             <h1 className="results-category">Accuracy</h1>
             <p className="results-value">{acc}%</p>
-            {acc < 70 && <p id="invalid-test-msg">*Test invalid due to low accuracy</p>}
+            {acc < 90 && <p id="invalid-test-msg">*Test invalid due to low accuracy</p>}
         </div>
     );
 }
