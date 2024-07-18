fb6a1d85d9a7ae515550d2f64578a878f66e9aa3
Rei Tan <tanreiyi@gmail.com>
2024-07-17 19:14:16 -0500

    rank enum

---
+++ src/utils/enum.js
@@ -0,0 +1,19 @@
+export const rank_enum = {
+    0: "Rock",
+    1: "LA traffic",
+    2: "Honey dripping", 
+    3: "Infant",
+    4: "Dung beetle",
+    5: "Fly", 
+    6: "Paper plane",
+    7: "Bowling ball",
+    8: "Plastic bag (caught in a strong breeze)",
+    9: "Refrigerator running",
+    10: "Popcorn kernel popping",
+    11: "行雲流水",
+    12: "Sneeze",
+    13: "Fastball",
+    14: "Sammie running" ,
+    15: "Rei typing",
+    16: "TypeReicer"
+};
