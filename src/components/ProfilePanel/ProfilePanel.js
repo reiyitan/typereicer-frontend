60112b8fc3f3ac1db84266301fb4e5082189f306
Rei Tan <tanreiyi@gmail.com>
2024-07-21 03:06:05 -0500

    base

--- src/components/ProfilePanel/ProfilePanel.js
+++ src/components/ProfilePanel/ProfilePanel.js
@@ -1,12 +1,36 @@
 import React from "react"; 
+import { useState, useEffect } from "react"; 
+import { useFirebase } from "../../ContextProviders";
 import "./ProfilePanel.css"; 
 
 export const ProfilePanel = () => {
+    const [userInfo, setUserInfo] = useState(null);
+    const { auth, get_user_info } = useFirebase();
+    useEffect(() => {
+        const uid = auth.currentUser.uid; 
+        const getInfo = async () => {
+            const userInfo = await get_user_info(uid);
+            setUserInfo(userInfo);
+        }
+        getInfo();
+    }, []);
+
+    if (!userInfo) {
+        return (
+            <div id="profile-panel" className="shadow">
+                <p id="loading-profile-msg">loading...</p>
+            </div>
+        ); 
+    }
+
     return (
         <div
             id="profile-panel"
+            className="shadow"
         >
-            
+            <div id="profile-header-container">
+                <h1 id="profile-header">{userInfo.username}{userInfo.username[userInfo.username.length - 1] === "s" ? "'" : "'s"} profile</h1>
+            </div>
         </div>
     );
 }
