4ee71f477f728168debc1160f053b3e1d00e7730
Rei Tan <tanreiyi@gmail.com>
2024-07-28 21:29:31 -0500

    implement leaderboard

--- src/components/LeaderboardPanel/LeaderboardPanel.js
+++ src/components/LeaderboardPanel/LeaderboardPanel.js
@@ -3,26 +3,43 @@ import { useEffect, useState } from "react";
 import "./LeaderboardPanel.css"; 
 import { useFirebase } from "../../ContextProviders";
 
-const Player = ({playerInfo}) => {
+const PlayerStanding = ({playerInfo, position}) => {
     return (
-        <div>
-            {playerInfo.username}
+        <div className="player-standing">
+            <span>{position}.</span>
+            <span>{playerInfo.username}</span>
+            <span>{playerInfo.average_wpm.toFixed(2)}</span>
+            <span>{playerInfo.average_acc.toFixed(2)}%</span>
         </div>
     )
 }
 
 export const LeaderboardPanel = () => {
-    const { get25_top10, get50_top10 } = useFirebase();
-    const [players25, setPlayers25] = useState([]);
-    const [players50, setPlayers50] = useState([]); 
+    const { get25_top10, get50_top10, get_overall_top10 } = useFirebase();
+    const [standings, setStandings] = useState([]);
+    const [mode, setMode] = useState("25");
 
     useEffect(() => {
         const fetchData = async () => {
-            setPlayers25(await get25_top10());
-            setPlayers50(await get50_top10());
+            switch (mode) {
+                case "25":
+                    setStandings(await get25_top10());
+                    break; 
+                case "50": 
+                    setStandings(await get50_top10());
+                    break; 
+                case "overall": 
+                    setStandings(await get_overall_top10());
+                    break;
+            }
         }
         fetchData();
-    }, []);
+    }, [mode]);
+
+    const handleClick = (newMode) => {
+        if (mode === newMode) return; 
+        setMode(newMode);
+    }
 
     return (
         <div 
@@ -30,25 +47,46 @@ export const LeaderboardPanel = () => {
             className="shadow"
         >
             <div id="leaderboard-header-container">
-                <h1 id="leaderboard-header">Leaderboards</h1>
+                <h1 id="leaderboard-header">Leaderboard</h1>
+                <div id="leaderboard-button-container">
+                    <button 
+                        className={"leaderboard-button " + (mode === "25" ? "leaderboard-button-active" : "leaderboard-button-inactive")}
+                        id="leaderboard-button-25"
+                        onClick={() => handleClick("25")}
+                    >
+                        25-word
+                    </button>
+                    <button 
+                        className={"leaderboard-button " + (mode === "50" ? "leaderboard-button-active" : "leaderboard-button-inactive")}
+                        onClick={() => handleClick("50")}
+                    >
+                        50-word
+                    </button>
+                    <button 
+                        className={"leaderboard-button " + (mode === "overall" ? "leaderboard-button-active" : "leaderboard-button-inactive")}
+                        id="leaderboard-button-overall"
+                        onClick={() => handleClick("overall")}
+                    >
+                        Overall
+                    </button>
+                </div>
             </div>
             <div id="standings-container">
-                <div id="standings-25">
+                <div id="standings">
+                    <div id="standings-category-header">
+                        <span className="standings-category">#</span>
+                        <span className="standings-category">username</span>
+                        <span className="standings-category">avg wpm</span>
+                        <span className="standings-category">avg acc</span>
+                    </div>
                     {
-                        players25.map((playerInfo, index) => (
-                            <Player 
+                        standings.length === 0
+                        ? <div id="no-standings-div">No standings for this mode yet</div>
+                        : standings.map((playerInfo, index) => (
+                            <PlayerStanding
                                 key={index}
                                 playerInfo={playerInfo}
-                            />
-                        ))
-                    }
-                </div>
-                <div id="standings-50">
-                    {
-                        players50.map((playerInfo, index) => (
-                            <Player 
-                                key={index}
-                                playerInfo={playerInfo}
+                                position={index + 1}
                             />
                         ))
                     }
